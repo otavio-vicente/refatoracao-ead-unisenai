5bba5c3f00c5b1adbf93b98f76aa15be3f79a3d8
otavio-vicente <otaviohvcnt@gmail.com>
2025-03-22 10:39:32 -0300

    Add algumas validações JS

--- js/script.js
+++ js/script.js
@@ -3,40 +3,46 @@
 var usuariosArray = []; 
 
 function adicionarusuario(){
-var nome = document.getElementById("nomeUsuario").value;
-var email = document.getElementById("emailUsuario").value;
+    var nome = document.getElementById("nomeUsuario").value;
+    var email = document.getElementById("emailUsuario").value;
+    var msgErro = document.getElementById("mensagemErro");
 
-if(nome == "" || email == ""){
-alert("Preencha todos os campos!"); return;
-}
+    if (nome === "" || email === "") {
+        msgErro.textContent = "Preencha todos os campos!";
+        msgErro.style.display = "block"; // Exibe a mensagem de erro
+        return;
+    }
 
-var usuario = { nome: nome, email: email };
-usuariosArray.push(usuario);
+    // Limpa a mensagem de erro se os campos estiverem preenchidos
+    msgErro.style.display = "none";
 
-console.log(usuariosArray);
+    var usuario = { nome: nome, email: email };
+    usuariosArray.push(usuario);
 
-var li = document.createElement("li");
-li.innerHTML = nome + " - " + email + " <button onclick='removeuser(\"" + email + "\")'>Remover</button>";
-document.getElementById("listausers").appendChild(li);
+    console.log(usuariosArray);
 
-document.getElementById("nomeUsuario").value = "";
-document.getElementById("emailUsuario").value = "";
+    var li = document.createElement("li");
+    li.innerHTML = nome + " - " + email + " <button onclick='removeuser(\"" + email + "\")'>Remover</button>";
+    document.getElementById("listausers").appendChild(li);
+
+    document.getElementById("nomeUsuario").value = "";
+    document.getElementById("emailUsuario").value = "";
 }
 
 function removeuser(email) {
-for (var i = 0; i < usuariosArray.length; i++) {
-    if (usuariosArray[i].email === email) {
-        usuariosArray.splice(i, 1);
+    for (var i = 0; i < usuariosArray.length; i++) {
+        if (usuariosArray[i].email === email) {
+            usuariosArray.splice(i, 1);
 
-        console.log("Hehehe, chablau, foi pro espeto!");
-        break;
+            console.log("Hehehe, chablau, foi pro espeto!");
+            break;
+        }
     }
-}
 
-document.getElementById("listausers").innerHTML = "";
-for (var i = 0; i < usuariosArray.length; i++) {
-    var li = document.createElement("li");
-    li.innerHTML = usuariosArray[i].nome + " - " + usuariosArray[i].email + " <button onclick='removeuser(\"" + usuariosArray[i].email + "\")'>Remover</button>";
-    document.getElementById("listausers").appendChild(li);
-}
+    document.getElementById("listausers").innerHTML = "";
+    for (var i = 0; i < usuariosArray.length; i++) {
+        var li = document.createElement("li");
+        li.innerHTML = usuariosArray[i].nome + " - " + usuariosArray[i].email + " <button onclick='removeuser(\"" + usuariosArray[i].email + "\")'>Remover</button>";
+        document.getElementById("listausers").appendChild(li);
+    }
 }
